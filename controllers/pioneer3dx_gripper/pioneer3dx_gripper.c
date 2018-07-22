bf5026b921ccb95c1c638b25e94c79e7a1419f28
khaibin <bkchow@yes.my>
2018-07-22 20:50:30 +0800

    Clean up unused codes

--- controllers/pioneer3dx_gripper/pioneer3dx_gripper.c
+++ controllers/pioneer3dx_gripper/pioneer3dx_gripper.c
@@ -71,32 +71,6 @@ static char *getColor(WbDeviceTag device){
         green += wb_camera_image_get_green(image, width, i, u);
       }
   }
-  //printf("%d,%d,%d\n", red, green, blue);
-  if (red == MAIN_COLOR && green == MAIN_COLOR)
-    return colors[3];
-  else if (red == MAIN_COLOR)
-    return colors[0];
-  else if (green == MAIN_COLOR)
-    return colors[1];
-  else if (blue == MAIN_COLOR)
-    return colors[2];
-  else
-    return colors[4];
-}
-
-static char *getColora(WbDeviceTag device){
-  int red = 0, green = 0, blue = 0;
-  int width = wb_camera_get_width(device);
-  int height = wb_camera_get_height(device);
-  const unsigned char *image = wb_camera_get_image(device);    
-  for (int i = 0; i < width; i++) {
-      for (int u = 0; u < height; u++) {
-        red += wb_camera_image_get_red(image, width, i, u);
-        blue += wb_camera_image_get_blue(image, width, i, u);
-        green += wb_camera_image_get_green(image, width, i, u);
-      }
-  }
-  printf("%d,%d,%d\n", red, green, blue);
   if (red == MAIN_COLOR && green == MAIN_COLOR)
     return colors[3];
   else if (red == MAIN_COLOR)
@@ -186,7 +160,6 @@ int main() {
   step(2.0);
   forward();
   while (wb_robot_step(time_step) != 1) {
-  //getColor(camera);
     if (tmp_color == colors[4]) {
       if (wb_distance_sensor_get_value(ds) < 400) {
         stop();
@@ -215,40 +188,5 @@ int main() {
       }
     }
   }
-  
-  
-
-  
-  /*
-
-  moveForwards(1.75);
-  step(2.0);
-  stop(0.5);
-  
-  turn(2.0);
-  step(2.720001);
-  turn(0);
-  step(1);
-  moveForwards(1.25);
-  step(5.5);
-  step(2.0);
-  stop(0.5);
-  moveFingers(0.01);
-  step(0.5);
-  lift(0.0);
-  step(0.5);
-  turn(2.0);
-  step(1.0);
-  moveForwards(1.25);
-  step(5.5);
-  turn(-1.5);
-  step(1.5);
-  stop(0.5);
-  lift(0.05);
-  step(0.5);
-  moveFingers(0.04);
-  step(0.5);
-  stop(0.5);
-  */
   return 0;
 }
